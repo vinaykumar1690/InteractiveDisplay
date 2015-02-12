42fa4da710e960a5fc3a79146fa6dd83724c51d2
Jeremy Fu <gengf@andrew.cmu.edu>
2015-02-11 16:54:00 -0800

    Caller & Callee trials

---
+++ caller.js
@@ -0,0 +1,27 @@
+// document.writeln("Hello, world!");
+
+var connection = new autobahn.Connection({
+   url: "ws://127.0.0.1:8080/ws",
+   realm: "realm1"
+});
+
+connection.onopen = function (session, details) 
+{
+   document.write("Connected in realm[" + session.realm + "]");
+   
+   console.log("Calling RPC");
+   
+   session.call('com.myapp.add2', [3, 18]).then(
+      function (res) {
+         console.log("add2() result:", res);
+      }
+   );
+};
+
+
+connection.onclose = function (reason, details) {
+   // handle connection lost
+   document.writeln("Failed to connect");
+}
+
+connection.open();
