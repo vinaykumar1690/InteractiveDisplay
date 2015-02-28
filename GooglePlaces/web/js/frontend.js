3241f5b9899575160741e6fa51f60400d4e75a35
vinaykumar1690 <vinaykumar1690@gmail.com>
2015-02-28 11:46:27 -0800

    autobahn successfully connected

--- GooglePlaces/web/js/frontend.js
+++ GooglePlaces/web/js/frontend.js
@@ -1,22 +1,21 @@
-// JavaScript source code
-//https://developers.google.com/maps/documentation/javascript/examples/place-search
-/* NOTE:
- * This code need to be optimized so it's not so replicaive(same functions repeated for map_left and map_right)
- */
+// var autobahn = require('autobahn');
+
+var connection = new autobahn.Connection({
+	url: 'ws://127.0.0.1:8080/ws',
+	realm: 'realm1'
+})
 
 var map_left;
 var city_left;
-
 var map_right;
 var city_right;
+var place_type = "bar";
 
-var place_type;
+connection.onopen = function(session) {
 
-//var infowindow;
+	console.log("frontend connected. Excellent");
 
-function initialize() {
-
-    /////////// MAP_LEFT
+    // MAP_LEFT
     // Specify an intial location
     city_left = "Mountain View";
     var location_left = new google.maps.LatLng(37.3944, -122.0759); // Mountain View
@@ -40,7 +39,7 @@ function initialize() {
     var svrs_left = new google.maps.places.PlacesService(map_left);
     svrs_left.nearbySearch(request_left, callback_left);
 
-    /////////// MAP_RIGHT
+    // MAP_RIGHT
     // Specify an intial location
     city_right = "Palo Alto";
     var location_right = new google.maps.LatLng(37.4292, -122.1381); // Palo Alto
@@ -64,9 +63,89 @@ function initialize() {
     var svrs_right = new google.maps.places.PlacesService(map_right);
     svrs_right.nearbySearch(request_right, callback_right);
 
+	var question;
 
-    // infowindow = new google.maps.InfoWindow();
+	/* Subscribe to .questions channel. This message is published
+	   when a new round of game starts. */
+	var onNewQuestion = function(args) {
+		question       = args[0];
+		ansOptsCounter = args[1];
+		console.log(question.question);
+		document.getElementById("questionText").innerHTML = question.question;
+		document.getElementById("questionText").setAttribute("seqno",question.seq);
+		document.getElementById("choiceTextA").innerHTML = question.options[0];
+		document.getElementById("choiceTextB").innerHTML = question.options[1];
+		document.getElementById("choiceTextC").innerHTML = question.options[2];
 
+		document.getElementById("pollsA").value = ansOptsCounter[0];
+		document.getElementById("pollsB").value = ansOptsCounter[1];
+		document.getElementById("pollsC").value = ansOptsCounter[2];
+	}
+
+	session.subscribe("edu.cmu.ipd.questions", onNewQuestion).then(
+      function (sub) {
+         console.log('subscribed to .questions');
+      },
+      function (err) {
+         console.log('failed to subscribe to .questions', err);
+      });
+
+	/* Subscribe to .onvote channel. This message is published
+	   when any client submit an answer */
+	var onNewVote = function(args) {
+		
+		var update = args[0];
+		var opt = update.optSEQ === 0 ? 'A' : (update.optSEQ === 1 ? 'B' : 'C');
+		document.getElementById("polls" + opt).value = update.value;
+	}
+
+	session.subscribe("edu.cmu.ipd.onvote", onNewVote).then(
+		function(sub) {
+			console.log("subscribe to .onvote");
+		},
+		function(err) {
+			console.log("fail to subscribe to .onvote");
+		});
+
+
+
+	/* Add oncClick listener to each button. Once a choice is clicked, it call RPC to backend */
+	
+	var formAnswer = function(qSEQ, optNum) {
+		var answer = {
+			qSEQ: qSEQ,
+			opt: optNum, 
+		}
+		return answer;
+	}
+
+	var submitAnswer = function(answer) {
+		session.call('edu.cmu.ipd.onpoll', [answer]).then(
+			function (res) {
+				console.log("successfully submit answer");
+			}
+		);
+	}
+
+	var choiceButtons = document.getElementById("pollContainer").
+		getElementsByTagName("button");
+	
+	for (var i = 0; i < choiceButtons.length; i++) {
+		choiceButtons[i].onclick = function(evt) {
+			var qSEQ = document.getElementById("questionText").getAttribute("seqno");
+			var choiceId = evt.target.id;
+			var optNum = choiceId === "A" ? 0 : (choiceId === "B" ? 1 : 2);
+			var ans = formAnswer(qSEQ, optNum);
+			submitAnswer(ans);
+			console.log("target.id: " + optNum + "/" + choiceId + "   Q#:" + qSEQ);
+		}
+	}
+
+	/* Retrieve the current question. This is only called only once upon loading the page */
+	session.call("edu.cmu.ipd.loadquestion", [0]).then(
+		function(res) {
+			onNewQuestion(res);
+		});
 }
 
 // result is of type <google.maps.places.PlaceResult>
@@ -102,15 +181,4 @@ function createMarker_right(place) {
     });
 }
 
-//google.maps.event.addListener(marker, 'click', setInfo);
-
-//var content = "<b>Name</b>:" + place.name +
-//                "<br><b>Rating</b>:" + place.rating;
-
-//function setInfo() {
-//    infowindow.setContent(content);
-//    infowindow.open(map_left, this);
-//};
-
-// Call initialize function.
-google.maps.event.addDomListener(window, 'load', initialize);
+connection.open();
