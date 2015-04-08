20006daa4b9353e594b1f26d93b0088674986316
YalaTest <yaleis@andrew.cmu.edu>
2015-04-08 13:57:27 -0700

    Display infoWindow for both paths, and in different color (blue & red)

--- google_translate_game/web/display/js/frontend.js
+++ google_translate_game/web/display/js/frontend.js
@@ -1,7 +1,6 @@
 var mapOptions = {
     zoom: 2,
     center: new google.maps.LatLng(23,0),
-    // mapTypeId: google.maps.MapTypeId.TERRAIN
 };
 
 var map = new google.maps.Map(document.getElementById('map_canvas'),
@@ -23,26 +22,33 @@ var loc_Rome = new google.maps.LatLng(41.9100, 12.5359);
 
 // List of countries
 var countries_1 = [
-	{name: "US", language: "English-US", location: loc_London},
-	{name: "Germany", language: "Germany", location: loc_Berlin},
-	{name: "Brazil", language: "Portuguese-Brazil", location: loc_Brasilia},
-    {name: "Congo", language: "Swahili", location: loc_Brazzaville},
-    {name: "Tunisia", language: "Arabic", location: loc_Tunis},
-	{name: "US", language: "English-US", location: loc_London}
+{name: "England", language: "English", location: loc_London},
+{name: "Germany", language: "Germany", location: loc_Berlin},
+{name: "Brazil", language: "Portuguese-Brazil", location: loc_Brasilia},
+{name: "Congo", language: "Swahili", location: loc_Brazzaville},
+{name: "Tunisia", language: "Arabic", location: loc_Tunis},
+{name: "England", language: "English", location: loc_London}
 ];
 
 var countries_2 = [
-    {name: "US", language: "English-US", location: loc_London},
-    {name: "Japan", language: "Japanese", location: loc_Tokyo},
-    {name: "Paris", language: "French", location: loc_Paris},
-    {name: "Delhi", language: "Hindi", location: loc_Delhi},
-    {name: "Seoul", language: "Korean", location: loc_Seoul},
-    {name: "Japan", language: "Japanese", location: loc_London},
+{name: "England", language: "English", location: loc_London},
+{name: "Japan", language: "Japanese", location: loc_Tokyo},
+{name: "Paris", language: "French", location: loc_Paris},
+{name: "Delhi", language: "Hindi", location: loc_Delhi},
+{name: "Seoul", language: "Korean", location: loc_Seoul},
+{name: "England", language: "English", location: loc_London}
 ];
 
 var lineSymbol = {
     path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
 };
+
+var markers_1 = [];
+var markers_2 = [];
+
+var infos = [{},{}];
+
+var path_length = 5;
 
 function initialize() {	
     // Define the LatLng coordinates for the polygon's path.
@@ -56,40 +62,58 @@ function initialize() {
 		scaledSize: new google.maps.Size(25, 25)
 	};
 
-	for (i = 0; i < countries_1.length; i++) {
-		image.url = './images/' + countries_1[i].name + '.png'; 
+// Markers for Path 1
+for (i = 0; i < countries_1.length; i++) {
+  image.url = './images/' + countries_1[i].name + '.png'; 
 		// console.log('image.url is ' + image.url);
 		var marker = new google.maps.Marker({
 			position: countries_1[i].location,
 			map: map,
 			// icon: image
 		});
-	}
+        markers_1.push(marker); // Add marker
+    }
+    info_contentStr_1 = '<div id="Infowindow_1" class="infowindow">' +
+    '<span id="InfowindowText_1" style="font-size: 2em;">'
+    + 'English' +
+    '</span>' +
+    '<div>';   
+    infos[0] = new google.maps.InfoWindow({
+        content: info_contentStr_1
+    });
+    infos[0].open(map, markers_1[0]);
 
-    for (i = 0; i < countries_2.length; i++) {
-        image.url = './images/' + countries_2[i].name + '.png'; 
+// Markers for Path 2
+for (i = 0; i < countries_2.length; i++) {
+    image.url = './images/' + countries_2[i].name + '.png'; 
         // console.log('image.url is ' + image.url);
         var marker = new google.maps.Marker({
             position: countries_2[i].location,
             map: map,
             // icon: image
         });
+        markers_2.push(marker); // Add marker        
     }
-	// console.log(countriesCoords);
-
-    // console.log('countries_1 is: ', countries_1);
-    
+    info_contentStr_2 = '<div id="Infowindow_2" class="infowindow">' +
+    '<span id="InfowindowText_2" style="font-size: 2em;">'
+    + 'English' +
+    '</span>' +
+    '<div>';      
+    infos[1] = new google.maps.InfoWindow({
+        content: info_contentStr_2
+    });
+    infos[1].open(map, markers_2[0]);
 
     drawPath2(countries_2);  
     drawPath1(countries_1);
 
- 
 } // End initialize()
 
 
 var drawPathClosure = function(pathNum) {
     var path_index = 0;
     var l = [];  
+    var markers = [];
 
     return function(countries) {
         console.log('Countries are: ', countries);
@@ -102,10 +126,14 @@ var drawPathClosure = function(pathNum) {
 
         console.log('%s to %s', countries[path_index].name, countries[path_index+1].name);
 
-        if (pathNum === 1)
-            {color = "#0000FF";}
-        else
-            {color = "#FF0000";}
+        if (pathNum === 1) {
+            color = "#0000FF";
+            markers = markers_1;
+        }
+        else {
+            color = "#FF0000";
+            markers = markers_2;
+        }
 
         l[path_index] = new google.maps.Polyline({
             path: [departure, departure],
@@ -115,7 +143,7 @@ var drawPathClosure = function(pathNum) {
             icons: [{
                 icon: lineSymbol,
                 offset: '100%',
-                repeat: '50px'
+                // repeat: '50px'
             }],
             geodesic: true, //set to false if you want straight line instead of arc
             map: map,
@@ -123,24 +151,48 @@ var drawPathClosure = function(pathNum) {
 
         var interval = setInterval(function() {
             step += 1;
-
             if (step > numSteps){
                 clearInterval(interval); // Stop the interval?
                 path_index++;
-                if (path_index < 5) {
-                    if (pathNum === 1) {drawPath1(countries);}
-                    else { drawPath2(countries)}
-                    
-                }
 
+                if (path_index < 5) {
+                    if (pathNum === 1) {
+                        drawPath1(countries);
+
+                    }
+                    else { 
+                        drawPath2(countries);
+                    }
+                    if (pathNum === 1)
+                    {
+                        document.getElementById("InfowindowText_1").innerHTML = countries[path_index].language;
+                        document.getElementById("InfowindowText_1").style.color = "#0000FF";
+                        infos[0].open(map, markers[path_index]); 
+                    }
+                    else
+                    { 
+                        document.getElementById("InfowindowText_2").innerHTML = countries[path_index].language;
+                        document.getElementById("InfowindowText_2").style.color = "#FF0000";
+                        infos[1].open(map, markers[path_index]); 
+                    }
+                }
+                else {
+                    // Display Final InfoWindow
+                    document.getElementById("InfowindowText_1").innerHTML = countries[path_index].language;
+                    infos[0].open(map, markers[path_index]); 
+
+                    document.getElementById("InfowindowText_2").innerHTML = countries[path_index].language;
+                    document.getElementById("InfowindowText_2").style.color = "#000000";
+                    infos[1].open(map, markers[path_index]);
+                }
                 console.log('Drew line: ', path_index);
                 console.log('step: %d, numSteps: %d', step, numSteps);
             }
             else {
-            var fraction = step/numSteps;
-            var progress = google.maps.geometry.spherical.interpolate(departure, arrival, fraction);
+                var fraction = step/numSteps;
+                var progress = google.maps.geometry.spherical.interpolate(departure, arrival, fraction);
 
-            l[path_index].setPath([departure, progress]);
+                l[path_index].setPath([departure, progress]);
             }
         }, timePerStep);
     }
